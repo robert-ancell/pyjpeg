001311eb65f0d9d59cdcb8098691229c225530c1
Robert Ancell <robert.ancell@gmail.com>
2024-08-31 21:29:13 +1200

    Make baseline generation function

--- jpeg.py
+++ jpeg.py
@@ -637,53 +637,55 @@ for i in range(len(samples)):
     samples[i] = round(samples[i] * 255 / max_value)
 
 
-quantization_luminance_table = [1] * 64  # FIXME: Using nothing at this point
-coefficients = make_dct_coefficients(
-    width, height, 8, samples, quantization_luminance_table
-)
-huffman_dc_table = make_dct_huffman_dc_table(coefficients)
-huffman_ac_table = make_dct_huffman_ac_table(coefficients)
-data = (
-    start_of_image()
-    + app0(density_unit=1, density=(72, 72))
-    + define_quantization_tables(
-        tables=[
-            QuantizationTable(destination=0, data=quantization_luminance_table),
-        ]
+def make_dct_baseline(width, samples):
+    height = len(samples) // width
+    quantization_table = [1] * 64  # FIXME: Using nothing at this point
+    coefficients = make_dct_coefficients(width, height, 8, samples, quantization_table)
+    dc_table = make_dct_huffman_dc_table(coefficients)
+    ac_table = make_dct_huffman_ac_table(coefficients)
+    return (
+        start_of_image()
+        + app0(density_unit=1, density=(72, 72))
+        + define_quantization_tables(
+            tables=[
+                QuantizationTable(destination=0, data=quantization_table),
+            ]
+        )
+        + start_of_frame_baseline(
+            width, height, [Component(id=1, quantization_table=0)]
+        )
+        + define_huffman_tables(
+            tables=[
+                HuffmanTable(
+                    table_class=HUFFMAN_CLASS_DC,
+                    destination=0,
+                    symbols_by_length=dc_table,
+                ),
+                HuffmanTable(
+                    table_class=HUFFMAN_CLASS_AC,
+                    destination=0,
+                    symbols_by_length=ac_table,
+                ),
+            ]
+        )
+        + start_of_scan(components=[ScanComponent.baseline(1, dc_table=0, ac_table=0)])
+        + huffman_dct_scan(
+            dc_table=dc_table,
+            ac_table=ac_table,
+            coefficients=coefficients,
+        )
+        + end_of_image()
     )
-    + start_of_frame_baseline(32, 32, [Component(id=1, quantization_table=0)])
-    + define_huffman_tables(
-        tables=[
-            HuffmanTable(
-                table_class=HUFFMAN_CLASS_DC,
-                destination=0,
-                symbols_by_length=huffman_dc_table,
-            ),
-            HuffmanTable(
-                table_class=HUFFMAN_CLASS_AC,
-                destination=0,
-                symbols_by_length=huffman_ac_table,
-            ),
-        ]
-    )
-    + start_of_scan(components=[ScanComponent.baseline(1, dc_table=0, ac_table=0)])
-    + huffman_dct_scan(
-        dc_table=huffman_dc_table,
-        ac_table=huffman_ac_table,
-        coefficients=coefficients,
-    )
-    + end_of_image()
-)
-open("out.jpg", "wb").write(data)
 
 
 def make_lossless(width, samples, predictor=1):
+    height = len(samples) // width
     values = make_lossless_values(predictor, 32, samples)
     table = make_lossless_huffman_table(values)
     return (
         start_of_image()
         + app0(density_unit=1, density=(72, 72))
-        + start_of_frame_lossless(32, 32, 8, [Component(id=1)])
+        + start_of_frame_lossless(width, height, 8, [Component(id=1)])
         + define_huffman_tables(
             tables=[
                 HuffmanTable(
@@ -704,6 +706,8 @@ def make_lossless(width, samples, predictor=1):
         + end_of_image()
     )
 
+
+open("baseline.jpg", "wb").write(make_dct_baseline(32, samples))
 
 for predictor in range(1, 8):
     open("lossless%d.jpg" % predictor, "wb").write(
