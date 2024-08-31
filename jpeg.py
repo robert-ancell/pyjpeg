f9b5d46f3ccd6fe6e3ec628c3576b756087993fd
Robert Ancell <robert.ancell@gmail.com>
2024-08-31 21:06:54 +1200

    Calculate zigzag

--- jpeg.py
+++ jpeg.py
@@ -542,85 +542,31 @@ def dct2d(values):
     return coefficients
 
 
-# [ 0,  1,  5,  6, 14, 15, 27, 28,
-#   2,  4,  7, 13, 16, 26, 29, 42,
-#   3,  8, 12, 17, 25, 30, 41, 43,
-#   9, 11, 18, 24, 31, 40, 44, 53,
-#  10, 19, 23, 32, 39, 45, 52, 54,
-#  20, 22, 33, 38, 46, 51, 55, 60,
-#  21, 34, 37, 47, 50, 56, 59, 61,
-#  35, 36, 48, 49, 57, 58, 62, 63]
 def zig_zag(coefficients):
     assert len(coefficients) == 64
-    reordered_coefficients = []
-    for i in [
-        0,
-        1,
-        8,
-        16,
-        9,
-        2,
-        3,
-        10,
-        17,
-        24,
-        32,
-        25,
-        18,
-        11,
-        4,
-        5,
-        12,
-        19,
-        26,
-        33,
-        40,
-        48,
-        41,
-        34,
-        27,
-        20,
-        13,
-        6,
-        7,
-        14,
-        21,
-        28,
-        35,
-        42,
-        49,
-        56,
-        57,
-        50,
-        43,
-        36,
-        29,
-        22,
-        15,
-        23,
-        30,
-        37,
-        44,
-        51,
-        58,
-        59,
-        52,
-        45,
-        38,
-        31,
-        39,
-        46,
-        53,
-        60,
-        61,
-        54,
-        47,
-        55,
-        62,
-        63,
-    ]:
-        reordered_coefficients.append(coefficients[i])
-    return reordered_coefficients
+    x = 0
+    y = 0
+    dx = 1
+    dy = -1
+    zz = []
+    for i in range(64):
+        zz.append(coefficients[y * 8 + x])
+        if x + dx >= 8:
+            y += 1
+            dx, dy = -1, 1
+        elif y + dy >= 8:
+            x += 1
+            dx, dy = 1, -1
+        elif x + dx < 0:
+            y += 1
+            dx, dy = 1, -1
+        elif y + dy < 0:
+            x += 1
+            dx, dy = -1, 1
+        else:
+            x += dx
+            y += dy
+    return zz
 
 
 def quantize(coefficients, quantization_table):
