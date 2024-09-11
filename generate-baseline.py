7b06acb112a72b6852a985ce5fea4da235b8bdcf
Robert Ancell <robert.ancell@gmail.com>
2024-09-11 23:10:47 +1200

    Simplify

--- generate-baseline.py
+++ generate-baseline.py
@@ -18,86 +18,52 @@ for s in raw_samples:
     cr_samples.append(cr)
 
 
-def make_dct_sequential_y(width, height, samples):
-    quantization_table = [1] * 64  # FIXME: Using nothing at this point
-    coefficients = make_dct_coefficients(width, height, 8, samples, quantization_table)
-    dc_table = make_dct_huffman_dc_table([coefficients])
-    ac_table = make_dct_huffman_ac_table([coefficients])
-    return (
-        start_of_image()
-        + jfif()
-        + define_quantization_tables(
-            tables=[
-                QuantizationTable(destination=0, data=quantization_table),
-            ]
-        )
-        + start_of_frame_baseline(
-            width, height, [Component(id=1, quantization_table=0)]
-        )
-        + define_huffman_tables(
-            tables=[
-                HuffmanTable.dc(0, dc_table),
-                HuffmanTable.ac(0, ac_table),
-            ]
-        )
-        + start_of_scan_sequential(
-            components=[ScanComponent(1, dc_table=0, ac_table=0)]
-        )
-        + huffman_dct_scan(
-            dc_table=dc_table,
-            ac_table=ac_table,
-            coefficients=coefficients,
-        )
-        + end_of_image()
-    )
-
-
-def make_dct_sequential_ycbcr(
-    width, height, y_samples, cb_samples, cr_samples, interleaved=False
-):
+def make_dct_sequential(width, height, samples, interleaved=False):
+    assert len(samples) in (1, 3)
     luminance_quantization_table = [1] * 64  # FIXME: Using nothing at this point
     chrominance_quantization_table = [1] * 64  # FIXME: Using nothing at this point
     y_coefficients = make_dct_coefficients(
         width, height, 8, y_samples, luminance_quantization_table
     )
-    cb_coefficients = make_dct_coefficients(
-        width, height, 8, cb_samples, chrominance_quantization_table
-    )
-    cr_coefficients = make_dct_coefficients(
-        width, height, 8, cr_samples, chrominance_quantization_table
-    )
     luminance_dc_table = make_dct_huffman_dc_table([y_coefficients])
     luminance_ac_table = make_dct_huffman_ac_table([y_coefficients])
-    chrominance_dc_table = make_dct_huffman_dc_table([cb_coefficients, cr_coefficients])
-    chrominance_ac_table = make_dct_huffman_ac_table([cb_coefficients, cr_coefficients])
+    quantization_tables = [
+        QuantizationTable(destination=0, data=luminance_quantization_table)
+    ]
+    components = [Component(id=1, quantization_table=0)]
+    huffman_tables = [
+        HuffmanTable.dc(0, luminance_dc_table),
+        HuffmanTable.ac(0, luminance_ac_table),
+    ]
+    if len(samples) > 1:
+        cb_coefficients = make_dct_coefficients(
+            width, height, 8, cb_samples, chrominance_quantization_table
+        )
+        cr_coefficients = make_dct_coefficients(
+            width, height, 8, cr_samples, chrominance_quantization_table
+        )
+        chrominance_dc_table = make_dct_huffman_dc_table(
+            [cb_coefficients, cr_coefficients]
+        )
+        chrominance_ac_table = make_dct_huffman_ac_table(
+            [cb_coefficients, cr_coefficients]
+        )
+        quantization_tables.append(
+            QuantizationTable(destination=1, data=chrominance_quantization_table)
+        )
+        components.append(Component(id=2, quantization_table=1))
+        components.append(Component(id=3, quantization_table=1))
+        huffman_tables.append(HuffmanTable.dc(1, chrominance_dc_table))
+        huffman_tables.append(HuffmanTable.ac(1, chrominance_ac_table))
+
     data = (
         start_of_image()
         + jfif()
-        + define_quantization_tables(
-            tables=[
-                QuantizationTable(destination=0, data=luminance_quantization_table),
-                QuantizationTable(destination=1, data=chrominance_quantization_table),
-            ]
-        )
-        + start_of_frame_baseline(
-            width,
-            height,
-            [
-                Component(id=1, quantization_table=0),
-                Component(id=2, quantization_table=1),
-                Component(id=3, quantization_table=1),
-            ],
-        )
-        + define_huffman_tables(
-            tables=[
-                HuffmanTable.dc(0, luminance_dc_table),
-                HuffmanTable.ac(0, luminance_ac_table),
-                HuffmanTable.dc(1, chrominance_dc_table),
-                HuffmanTable.ac(1, chrominance_ac_table),
-            ]
-        )
+        + define_quantization_tables(tables=quantization_tables)
+        + start_of_frame_baseline(width, height, components)
+        + define_huffman_tables(tables=huffman_tables)
     )
-    if interleaved:
+    if interleaved and len(samples) > 1:
         data += start_of_scan_sequential(
             components=[
                 ScanComponent(1, dc_table=0, ac_table=0),
@@ -124,51 +90,52 @@ def make_dct_sequential_ycbcr(
             ],
         )
     else:
-        data += (
-            start_of_scan_sequential(
-                components=[
-                    ScanComponent(1, dc_table=0, ac_table=0),
-                ]
-            )
-            + huffman_dct_scan(
-                dc_table=luminance_dc_table,
-                ac_table=luminance_ac_table,
-                coefficients=y_coefficients,
-            )
-            + start_of_scan_sequential(
-                components=[
-                    ScanComponent(2, dc_table=1, ac_table=1),
-                ]
-            )
-            + huffman_dct_scan(
-                dc_table=chrominance_dc_table,
-                ac_table=chrominance_ac_table,
-                coefficients=cb_coefficients,
-            )
-            + start_of_scan_sequential(
-                components=[
-                    ScanComponent(3, dc_table=1, ac_table=1),
-                ]
-            )
-            + huffman_dct_scan(
-                dc_table=chrominance_dc_table,
-                ac_table=chrominance_ac_table,
-                coefficients=cr_coefficients,
-            )
+        data += start_of_scan_sequential(
+            components=[
+                ScanComponent(1, dc_table=0, ac_table=0),
+            ]
+        ) + huffman_dct_scan(
+            dc_table=luminance_dc_table,
+            ac_table=luminance_ac_table,
+            coefficients=y_coefficients,
         )
+
+        if len(samples) > 1:
+            data += (
+                start_of_scan_sequential(
+                    components=[
+                        ScanComponent(2, dc_table=1, ac_table=1),
+                    ]
+                )
+                + huffman_dct_scan(
+                    dc_table=chrominance_dc_table,
+                    ac_table=chrominance_ac_table,
+                    coefficients=cb_coefficients,
+                )
+                + start_of_scan_sequential(
+                    components=[
+                        ScanComponent(3, dc_table=1, ac_table=1),
+                    ]
+                )
+                + huffman_dct_scan(
+                    dc_table=chrominance_dc_table,
+                    ac_table=chrominance_ac_table,
+                    coefficients=cr_coefficients,
+                )
+            )
     data += end_of_image()
     return data
 
 
 open("jpeg/baseline/32x32x8_y.jpg", "wb").write(
-    make_dct_sequential_y(width, height, y_samples)
+    make_dct_sequential(width, height, [y_samples])
 )
 open("jpeg/baseline/32x32x8_ycbcr.jpg", "wb").write(
-    make_dct_sequential_ycbcr(width, height, y_samples, cb_samples, cr_samples)
+    make_dct_sequential(width, height, [y_samples, cb_samples, cr_samples])
 )
 open("jpeg/baseline/32x32x8_ycbcr_interleaved.jpg", "wb").write(
-    make_dct_sequential_ycbcr(
-        width, height, y_samples, cb_samples, cr_samples, interleaved=True
+    make_dct_sequential(
+        width, height, [y_samples, cb_samples, cr_samples], interleaved=True
     )
 )
 # version 1.1
