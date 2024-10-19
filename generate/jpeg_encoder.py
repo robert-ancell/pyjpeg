ecd5940528d18517676cc0372f59c5c90d8df5fc
Robert Ancell <robert.ancell@gmail.com>
2024-10-20 10:37:27 +1300

    Can encode arithmetic

--- generate/jpeg_encoder.py
+++ generate/jpeg_encoder.py
@@ -1,7 +1,7 @@
 import struct
 
+import arithmetic
 import jpeg
-
 from jpeg_segments import *
 from jpeg_marker import *
 
@@ -91,6 +91,16 @@ class Encoder:
         data += struct.pack("BBB", sos.ss, sos.se, a)
         self.encode_segment(data)
 
+    def encode_huffman_dct_scan(self, scan):
+        encoder = HuffmanDCTScanEncoder()
+        for data_unit in scan.data_units:
+            encoder.write_data_unit(data_unit, 0, 0)
+
+    def encode_arithmetic_dct_scan(self, scan):
+        encoder = ArithmeticDCTScanEncoder()
+        for data_unit in scan.data_units:
+            encoder.write_data_unit(data_unit, 0, 0)
+
     def encode_rst(self, rst):
         self.encode_marker(MARKER_RST0 + rst.n)
 
@@ -123,9 +133,9 @@ class Encoder:
             elif isinstance(segment, StartOfScan):
                 self.encode_sos(segment)
             elif isinstance(segment, HuffmanDCTScan):
-                pass
+                self.encode_huffman_dct_scan(segment)
             elif isinstance(segment, ArithmeticDCTScan):
-                pass
+                self.encode_arithmetic_dct_scan(segment)
             elif isinstance(segment, Restart):
                 self.encode_rst(segment)
             elif isinstance(segment, DefineNumberOfLines):
@@ -147,6 +157,15 @@ class ArithmeticDCTScanEncoder:
         self.conditioning_bounds = conditioning_bounds
         self.kx = kx
 
+        def make_states(count):
+            states = []
+            for _ in range(count):
+                states.append(arithmetic.State())
+            return states
+
+        self.encoder = arithmetic.Encoder()
+        self.prev_dc = 0
+        self.prev_dc_diff = 0
         self.dc_non_zero = make_states(5)
         self.dc_sign = make_states(5)
         self.dc_sp = make_states(5)
@@ -162,17 +181,23 @@ class ArithmeticDCTScanEncoder:
         self.ac_high_mstates = make_states(14)
 
     def write_data_unit(self, data_unit, dc_table, ac_table):
-        zz_data_unit = jpeg.ig_zag(data_unit)
+        zz_data_unit = jpeg.zig_zag(data_unit)
 
         k = self.spectral_selection[0]
         while k <= self.spectral_selection[1]:
             if k == 0:
-                # FIXME: prev_dc_diff
-                self.write_dc(zz_data_unit[k])
+                dc = zz_data_unit[k]
+                dc_diff = dc - self.prev_dc
+                self.prev_dc = dc
+                self.write_dc(dc_diff, self.prev_dc_diff)
+                self.prev_dc_diff = dc_diff
                 k += 1
             else:
                 run_length = 0
-                while zz_data_unit[k] == 0 and k <= self.spectral_selection[1]:
+                while (
+                    k + run_length <= self.spectral_selection[1]
+                    and zz_data_unit[k + run_length] == 0
+                ):
                     run_length += 1
                 if k + run_length > self.spectral_selection[1]:
                     self.encoder.write_bit(self.ac_end_of_block[k - 1], 1)
@@ -180,21 +205,22 @@ class ArithmeticDCTScanEncoder:
                 else:
                     self.encoder.write_bit(self.ac_end_of_block[k - 1], 0)
                     for _ in range(run_length):
-                        self.decoder.write_bit(self.ac_non_zero[k - 1], 0)
+                        self.encoder.write_bit(self.ac_non_zero[k - 1], 0)
                         k += 1
+                    self.encoder.write_bit(self.ac_non_zero[k - 1], 1)
                     self.write_ac(zz_data_unit[k], k, self.kx[ac_table])
                     k += 1
 
-    def write_dc(self, dc, prev_dc_diff):
+    def write_dc(self, dc_diff, prev_dc_diff):
         # FIXME: Classify prev_dc_diff
         c = 0
 
-        if dc == 0:
-            self.decoder.write_bit(self.dc_non_zero[c], 0)
+        if dc_diff == 0:
+            self.encoder.write_bit(self.dc_non_zero[c], 0)
             return
-        self.decoder.write_bit(self.dc_non_zero[c], 1)
+        self.encoder.write_bit(self.dc_non_zero[c], 1)
 
-        if dc > 0:
+        if dc_diff > 0:
             sign = 1
             magnitude = ac
             self.encoder.write_bit(self.dc_sign[c], 0)
@@ -210,7 +236,7 @@ class ArithmeticDCTScanEncoder:
                 return
 
         width = 0
-        while (dc >> width) != 0:
+        while (dc_diff >> width) != 0:
             width += 1
 
         for _ in range(width):
@@ -261,8 +287,12 @@ class ArithmeticDCTScanEncoder:
         for i in range(width - 1, -1, -1):
             self.encoder.write_bit(mstates[width - 2], (magnitude >> i) & 0x1)
 
+    def get_data():
+        self.encoder.flush()
+        return self.encoder.data
 
-class HuffmanDCTScanEnoder:
+
+class HuffmanDCTScanEncoder:
     def __init__(
         self,
         spectral_selection=(0, 63),
@@ -272,16 +302,59 @@ class HuffmanDCTScanEnoder:
         self.spectral_selection = spectral_selection
         self.dc_tables = dc_tables
         self.ac_tables = ac_tables
+        self.prev_dc = 0
+        self.bits = []
+
+    def write_data_unit(self, data_unit, dc_table, ac_table):
+        zz_data_unit = jpeg.ig_zag(data_unit)
+
+        k = self.spectral_selection[0]
+        while k <= self.spectral_selection[1]:
+            if k == 0:
+                dc = zz_data_unit[k]
+                dc_diff = dc - self.prev_dc
+                self.prev_dc = dc
+                self.write_dc(dc)
+                k += 1
+            else:
+                run_length = 0
+                while (
+                    k + run_length <= self.spectral_selection[1]
+                    and zz_data_unit[k + run_length] == 0
+                ):
+                    run_length += 1
+                if run_length >= 16:
+                    self.write_ac(15, 0)
+                    k += 16
+                elif k + run_length > self.spectral_selection[1]:
+                    self.write_eob()
+                    k += run_length
+                else:
+                    self.write_ac(run_length, zz_data_unit[k])
+                    k += 1
+
+    def write_dc(self, dc_diff):
+        pass
+
+    def write_ac(self, run_length, ac):
+        pass
+
+    def get_data(self):
+        # FIXME: Append 1 bits
+        # FIXME: Pack into bytes
+        return b""
 
 
 encoder = Encoder(
     [
         StartOfImage(),
-        DefineQuantizationTables([QuantizationTable(8, 0, [1] * 64)]),
-        StartOfFrame(8, 8, 8, 8, [FrameComponent(1, (1, 1), 0)]),
+        DefineQuantizationTables([QuantizationTable(0, 0, [1] * 64)]),
+        StartOfFrame(9, 8, 8, 8, [FrameComponent(1, (1, 1), 0)]),
         StartOfScan([StreamComponent(1, 0, 0)], 0, 63, 0, 0),
+        ArithmeticDCTScan([[0] * 64]),
         EndOfImage(),
     ]
 )
 encoder.encode()
 print(encoder.data)
+open("test.jpg", "wb").write(encoder.data)
