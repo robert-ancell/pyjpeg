65c7d058a308d1aac877e5f7c74222b66573cf6e
Robert Ancell <robert.ancell@gmail.com>
2026-04-25 20:24:46 +1200

    Add lossless decoder

--- src/jpeg/lossless.py
+++ src/jpeg/lossless.py
@@ -1,3 +1,4 @@
+# FIXME: Make private
 def predict(predictor, a, b, c):
     if predictor == 1:
         return a
@@ -22,21 +23,7 @@ def make_data_units(width, samples, precision=8, predictor=1):
     height = len(samples) // width
     for y in range(height):
         for x in range(width):
-            a = samples[y * width + (x - 1)] if x > 0 else 0
-
-            if y == 0:
-                if x == 0 and y == 0:
-                    p = 1 << (precision - 1)
-                else:
-                    p = samples[y * width + x - 1]
-            else:
-                if x == 0:
-                    p = samples[y * width + x - width]
-                else:
-                    a = samples[y * width + x - 1]
-                    b = samples[y * width + x - width]
-                    c = samples[y * width + x - width - 1]
-                    p = predict(predictor, a, b, c)
+            p = _predict(width, samples, x, y, precision=precision, predictor=predictor)
             diff = samples[y * width + x] - p
             if diff > 32768:
                 diff -= 65536
@@ -44,3 +31,47 @@ def make_data_units(width, samples, precision=8, predictor=1):
                 diff += 65536
             data_units.append(diff)
     return data_units
+
+
+def get_samples(width, data_units, precision=8, predictor=1):
+    samples = []
+    height = len(data_units) // width
+    for y in range(height):
+        for x in range(width):
+            p = _predict(width, samples, x, y, precision=precision, predictor=predictor)
+            diff = data_units[y * width + x]
+            s = p + diff
+            if s > 32767:
+                s -= 65536
+            if s < -32768:
+                s += 65536
+            samples.append(s)
+    return samples
+
+
+def _predict(samples_per_line, samples, x, y, precision=8, predictor=1):
+    a = samples[y * samples_per_line + (x - 1)] if x > 0 else 0
+
+    if y == 0:
+        if x == 0 and y == 0:
+            return 1 << (precision - 1)
+        else:
+            return samples[y * samples_per_line + x - 1]
+    else:
+        if x == 0:
+            return samples[y * samples_per_line + x - samples_per_line]
+        else:
+            a = samples[y * samples_per_line + x - 1]
+            b = samples[y * samples_per_line + x - samples_per_line]
+            c = samples[y * samples_per_line + x - samples_per_line - 1]
+            return predict(predictor, a, b, c)
+
+
+if __name__ == "__main__":
+    import random
+
+    for predictor in [1, 2, 3, 4, 5, 6, 7]:
+        samples = [random.randint(0, 255) for _ in range(64)]
+        data_units = make_data_units(8, samples, predictor=predictor)
+        samples2 = get_samples(8, data_units, predictor=predictor)
+        assert samples == samples2
