d2c79557383ac94d4e886b0df61d764692827fed
Robert Ancell <robert.ancell@gmail.com>
2026-04-25 22:19:09 +1200

    Fix scan en/decoder

--- src/jpeg/scan.py
+++ src/jpeg/scan.py
@@ -5,7 +5,7 @@ class Writer:
         self.bit_count = 0
 
     def write_bit(self, bit):
-        self.data |= bit << self.bit_count
+        self.data |= bit << (7 - self.bit_count)
         self.bit_count += 1
         if self.bit_count == 8:
             self.writer.write_u8(self.data)
@@ -14,12 +14,13 @@ class Writer:
             self.data = 0
             self.bit_count = 0
 
-    def flush(self):
+    # FIXME: Check what pad bit should be used
+    def flush(self, pad_bit=0):
         if self.bit_count == 0:
             return
         n_padding = 8 - self.bit_count
         for i in range(n_padding):
-            self.write_bit(0)
+            self.write_bit(pad_bit)
 
 
 class Reader:
@@ -30,14 +31,36 @@ class Reader:
 
     def read_bit(self):
         if self.bit_count == 0:
-            data = self.reader.peek(1)
+            data = self.reader.peek(1)[0]
             if data == 0xFF:
                 if self.reader.peek(2)[1] != 0:
                     raise Exception("End of stream")
-                self.data = 0
+                self.data = self.reader.read_u8()
+                self.reader.read_u8()
             else:
                 self.data = self.reader.read_u8()
             self.bit_count = 8
-        bit = (self.data >> (7 - self.bit_count)) & 1
+        bit = (self.data >> (self.bit_count - 1)) & 1
         self.bit_count -= 1
         return bit
+
+
+if __name__ == "__main__":
+    import jpeg.reader
+    import jpeg.writer
+
+    bits = [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1]
+
+    writer = jpeg.BufferedWriter()
+    scan_writer = Writer(writer)
+    for bit in bits:
+        scan_writer.write_bit(bit)
+    scan_writer.flush()
+
+    assert writer.data == b"\xaa\xff\x00\x0f"
+
+    reader = jpeg.BufferedReader(writer.data)
+    scan_reader = Reader(reader)
+    for i in range(len(bits)):
+        bit = scan_reader.read_bit()
+        assert bit == bits[i]
