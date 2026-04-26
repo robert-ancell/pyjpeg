a6eb5dc1e69c3bc7c43aacb7c5fe6a7d9d6c435b
Robert Ancell <robert.ancell@gmail.com>
2026-04-26 21:50:56 +1200

    arith lossless decode

--- src/jpeg/arithmetic_lossless_scan.py
+++ src/jpeg/arithmetic_lossless_scan.py
@@ -1,22 +1,36 @@
 import jpeg.arithmetic
 import jpeg.arithmetic_scan
 import jpeg.lossless
+import jpeg.stream
 
 
 class ArithmeticLosslessScanComponent:
     def __init__(self, conditioning_bounds=(0, 1)):
         self.conditioning_bounds = conditioning_bounds
 
+    def __eq__(self, other):
+        return (
+            isinstance(other, ArithmeticLosslessScanComponent)
+            and other.conditioning_bounds == self.conditioning_bounds
+        )
+
 
 class ArithmeticLosslessScan:
-    def __init__(self, samples_per_line, samples, components, precision=8, predictor=1):
+    def __init__(
+        self,
+        samples_per_line: int,
+        samples,
+        components,
+        precision: int = 8,
+        predictor: int = 1,
+    ):
         self.samples_per_line = samples_per_line
         self.samples = samples
         self.components = components
         self.precision = precision
         self.predictor = predictor
 
-    def encode(self, writer):
+    def encode(self, writer: jpeg.stream.Writer):
         writer = Writer(writer)
 
         # FIXME: Store samples per component
@@ -48,21 +62,76 @@ class ArithmeticLosslessScan:
                 )
 
                 writer.write_data_unit(
-                    self.components[component_index].conditioning_bounds,
-                    left_data_unit,
-                    above_data_unit,
                     data_unit,
+                    left_data_unit=left_data_unit,
+                    above_data_unit=above_data_unit,
+                    conditioning_bounds=self.components[
+                        component_index
+                    ].conditioning_bounds,
                 )
 
         writer.flush()
 
-    def decode(reader, samples_per_line, components, precision=8, predictor=1):
+    def decode(
+        reader: jpeg.stream.Reader,
+        samples_per_line: int,
+        components,
+        number_of_lines: int = 0,
+        precision: int = 8,
+        predictor: int = 1,
+    ):
+        component_data_units = [[] for _ in range(len(components))]
         reader = Reader(reader)
-        # FIXME
+        # FIXME: Support undefined number of lines
+        assert number_of_lines > 0
+        for y in range(number_of_lines):
+            for x in range(samples_per_line):
+                for component_index in range(len(components)):
+                    left_data_unit = (
+                        component_data_units[component_index][
+                            y * samples_per_line + x - 1
+                        ]
+                        if x > 0
+                        else 0
+                    )
+                    above_data_unit = (
+                        component_data_units[component_index][
+                            y * samples_per_line + x - samples_per_line
+                        ]
+                        if y > 0
+                        else 0
+                    )
+
+                    data_unit = reader.read_data_unit(
+                        left_data_unit=left_data_unit,
+                        above_data_unit=above_data_unit,
+                        conditioning_bounds=components[
+                            component_index
+                        ].conditioning_bounds,
+                    )
+                    component_data_units[component_index].append(data_unit)
+
+        component_samples = []
+        for component_index, component in enumerate(components):
+            component_samples.append(
+                jpeg.lossless.decode(
+                    samples_per_line,
+                    component_data_units[component_index],
+                    precision=precision,
+                    predictor=predictor,
+                )
+            )
+
+        samples = []
+        for _ in range(number_of_lines):
+            for _ in range(samples_per_line):
+                for component_index in range(len(components)):
+                    samples.append(component_samples[component_index].pop(0))
+
         return ArithmeticLosslessScan(
-            reader.samples_per_line,
+            samples_per_line,
+            samples,
             components,
-            [],
             precision=precision,
             predictor=predictor,
         )
@@ -84,9 +153,11 @@ class Writer:
         self.small_mstates = make_states(14)
         self.large_mstates = make_states(14)
 
-    def write_data_unit(self, conditioning_bounds, left_diff, above_diff, data_unit):
-        ca = jpeg.arithmetic_scan.classify_dc(conditioning_bounds, left_diff)
-        cb = jpeg.arithmetic_scan.classify_dc(conditioning_bounds, above_diff)
+    def write_data_unit(
+        self, data_unit, left_data_unit=0, above_data_unit=0, conditioning_bounds=(0, 1)
+    ):
+        ca = jpeg.arithmetic_scan.classify_dc(conditioning_bounds, left_data_unit)
+        cb = jpeg.arithmetic_scan.classify_dc(conditioning_bounds, above_data_unit)
         c = ca * 5 + cb
         if (
             cb == jpeg.arithmetic_scan.Classification.LARGE_POSITIVE
@@ -115,7 +186,7 @@ class Reader:
     def __init__(self, reader):
         self.reader = jpeg.arithmetic_scan.Reader(reader)
 
-        def make_states(self, count):
+        def make_states(count):
             return [jpeg.arithmetic.State() for _ in range(count)]
 
         self.non_zero = make_states(25)
@@ -127,9 +198,11 @@ class Reader:
         self.small_mstates = make_states(14)
         self.large_mstates = make_states(14)
 
-    def read_data_unit(self, conditioning_bounds, left_diff, above_diff):
-        ca = jpeg.arithmetic_scan.classify_dc(conditioning_bounds, left_diff)
-        cb = jpeg.arithmetic_scan.classify_dc(conditioning_bounds, above_diff)
+    def read_data_unit(
+        self, left_data_unit=0, above_data_unit=0, conditioning_bounds=(0, 1)
+    ):
+        ca = jpeg.arithmetic_scan.classify_dc(conditioning_bounds, left_data_unit)
+        cb = jpeg.arithmetic_scan.classify_dc(conditioning_bounds, above_data_unit)
         c = ca * 5 + cb
         if (
             cb == jpeg.arithmetic_scan.Classification.LARGE_POSITIVE
@@ -143,3 +216,26 @@ class Reader:
         return self.reader.read_dc(
             self.non_zero[c], self.sign[c], self.sp[c], self.sn[c], xstates, mstates
         )
+
+
+if __name__ == "__main__":
+    import random
+
+    samples = [random.randint(0, 255) for _ in range(64)]
+    scan = ArithmeticLosslessScan(
+        8,
+        samples,
+        [ArithmeticLosslessScanComponent()],
+    )
+    writer = jpeg.stream.BufferedWriter()
+    scan.encode(writer)
+
+    reader = jpeg.stream.BufferedReader(writer.data)
+    scan2 = ArithmeticLosslessScan.decode(
+        reader, 8, [ArithmeticLosslessScanComponent()], number_of_lines=8
+    )
+    assert scan2.samples_per_line == 8
+    assert scan2.samples == samples
+    assert scan2.components == [ArithmeticLosslessScanComponent()]
+    assert scan2.precision == 8
+    assert scan2.predictor == 1
