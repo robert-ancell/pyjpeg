daa41b631ed70e5f3c72d20460dde01950ae04fc
Robert Ancell <robert.ancell@gmail.com>
2026-05-01 14:15:23 +1200

    Complete DC successive tests

--- src/jpeg/arithmetic_dct_dc_successive_scan.py
+++ src/jpeg/arithmetic_dct_dc_successive_scan.py
@@ -1,29 +1,28 @@
 import jpeg.arithmetic
-import jpeg.segment
+import jpeg.io
 
 
 class ArithmeticDCTDCSuccessiveScan:
     def __init__(self, data_units, point_transform=0):
-        # FIXME: Rename to dc_values
         self.data_units = data_units
         self.point_transform = point_transform
 
     def write(self, writer: jpeg.io.Writer):
         writer = jpeg.arithmetic.Writer(writer)
-        prev_dc = 0
         for data_unit in self.data_units:
-            dc = data_unit[0]
-            writer.write_fixed_bit((dc >> self.point_transform) & 0x1)
+            writer.write_fixed_bit((data_unit[0] >> self.point_transform) & 0x1)
 
         writer.flush()
 
-    def read(reader: jpeg.io.Reader, number_of_data_units, point_transform=0):
-        reader = jpeg.arithmetic.Reader(reader)
-        prev_dc = 0
-        for _ in range(number_of_data_units):
-            bit = reader.read_fixed_bit() << point_transform
+    def read(reader: jpeg.io.Reader, data_units, point_transform=0):
+        scan_reader = jpeg.arithmetic.Reader(reader)
+        updated_data_units = []
+        for data_unit in data_units:
+            updated_data_unit = data_unit[:]
+            updated_data_unit[0] += scan_reader.read_fixed_bit() << point_transform
+            updated_data_units.append(updated_data_unit)
         return ArithmeticDCTDCSuccessiveScan(
-            data_units, point_transform=point_transform
+            updated_data_units, point_transform=point_transform
         )
 
 
@@ -32,15 +31,30 @@ if __name__ == "__main__":
 
     import jpeg.dct
 
-    samples = [random.randint(0, 255) for _ in range(64)]
-    data_units = [jpeg.dct.quantize(jpeg.dct.fdct(samples), [1] * 64)]
+    data_units = []
+    for _ in range(4):
+        samples = [random.randint(0, 255) for _ in range(64)]
+        data_units.append(jpeg.dct.quantize(jpeg.dct.fdct(samples), [1] * 64))
 
     writer = jpeg.io.BufferedWriter()
-    scan = ArithmeticDCTDCSuccessiveScan(data_units)
+    scan = ArithmeticDCTDCSuccessiveScan(data_units, point_transform=3)
     scan.write(writer)
 
-    reader = jpeg.io.BufferedReader(writer.data)
-    scan2 = ArithmeticDCTDCSuccessiveScan.read(reader, 1)
+    def mask_coefficients(data_units, mask):
+        masked_data_units = []
+        for data_unit in data_units:
+            masked_data_unit = [0] * 64
+            masked_data_unit[0] = data_unit[0] & mask
+        return masked_data_units
 
-    # FIXME
-    # assert scan2.data_units == data_units
+    # Feed in data units with bits removed
+    approximate_data_units = mask_coefficients(data_units, 0xFFF0)
+
+    # Expect next bit to be reconstructed
+    expected_data_units = mask_coefficients(data_units, 0xFFF8)
+
+    reader = jpeg.io.BufferedReader(writer.data)
+    scan2 = ArithmeticDCTDCSuccessiveScan.read(
+        reader, approximate_data_units, point_transform=3
+    )
+    assert scan2.data_units == expected_data_units
--- src/jpeg/huffman_dct_dc_successive_scan.py
+++ src/jpeg/huffman_dct_dc_successive_scan.py
@@ -1,9 +1,8 @@
+import jpeg.io
 import jpeg.scan
-import jpeg.segment
 
 
 class HuffmanDCTDCSuccessiveScan:
-    # FIXME: Don't take whole data_units, just DC bits
     def __init__(self, data_units, point_transform=0):
         self.data_units = data_units
         self.point_transform = point_transform
@@ -11,23 +10,22 @@ class HuffmanDCTDCSuccessiveScan:
     def write(self, writer):
         scan_writer = jpeg.scan.Writer(writer)
 
-        prev_dc = 0
         for data_unit in self.data_units:
-            dc = data_unit[0]
-            bit = (dc >> self.point_transform) & 0x1
+            bit = (data_unit[0] >> self.point_transform) & 0x1
             scan_writer.write_bit(bit)
 
         scan_writer.flush(pad_bit=1)
 
-    def read(reader: jpeg.io.Reader, number_of_data_units, point_transform=0):
+    def read(reader: jpeg.io.Reader, data_units, point_transform=0):
         scan_reader = jpeg.scan.Reader(reader)
-        data_units = []
-        for _ in range(number_of_data_units):
-            bit = scan_reader.read_bit()
-            dc = bit << point_transform
-            data_unit = [dc] + [0] * 63
-            data_units.append(data_unit)
-        return HuffmanDCTDCSuccessiveScan(data_units, point_transform=point_transform)
+        updated_data_units = []
+        for data_unit in data_units:
+            updated_data_unit = data_unit[:]
+            updated_data_unit[0] += scan_reader.read_bit() << point_transform
+            updated_data_units.append(updated_data_unit)
+        return HuffmanDCTDCSuccessiveScan(
+            updated_data_units, point_transform=point_transform
+        )
 
 
 if __name__ == "__main__":
@@ -35,15 +33,30 @@ if __name__ == "__main__":
 
     import jpeg.dct
 
-    samples = [random.randint(0, 255) for _ in range(64)]
-    data_units = [jpeg.dct.quantize(jpeg.dct.fdct(samples), [1] * 64)]
+    data_units = []
+    for _ in range(4):
+        samples = [random.randint(0, 255) for _ in range(64)]
+        data_units.append(jpeg.dct.quantize(jpeg.dct.fdct(samples), [1] * 64))
 
     writer = jpeg.io.BufferedWriter()
-    scan = HuffmanDCTDCSuccessiveScan(data_units)
+    scan = HuffmanDCTDCSuccessiveScan(data_units, point_transform=3)
     scan.write(writer)
 
-    reader = jpeg.io.BufferedReader(writer.data)
-    scan2 = HuffmanDCTDCSuccessiveScan.read(reader, 1)
+    def mask_coefficients(data_units, mask):
+        masked_data_units = []
+        for data_unit in data_units:
+            masked_data_unit = [0] * 64
+            masked_data_unit[0] = data_unit[0] & mask
+        return masked_data_units
 
-    # FIXME
-    # assert scan2.data_units == data_units
+    # Feed in data units with bits removed
+    approximate_data_units = mask_coefficients(data_units, 0xFFF0)
+
+    # Expect next bit to be reconstructed
+    expected_data_units = mask_coefficients(data_units, 0xFFF8)
+
+    reader = jpeg.io.BufferedReader(writer.data)
+    scan2 = HuffmanDCTDCSuccessiveScan.read(
+        reader, approximate_data_units, point_transform=3
+    )
+    assert scan2.data_units == expected_data_units
