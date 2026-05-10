e30c58bac03c6e0ded0304448e9e34b04dcd21b0
Robert Ancell <robert.ancell@gmail.com>
2026-05-10 16:44:29 +0200

    read method is different for different classes

--- src/jpeg/segment.py
+++ src/jpeg/segment.py
@@ -4,7 +4,3 @@ import jpeg.io
 class Segment:
     def write(self, writer: jpeg.io.Writer):
         raise NotImplementedError
-
-    @classmethod
-    def read(cls, reader: jpeg.io.Reader):
-        raise NotImplementedError
