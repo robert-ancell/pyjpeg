8d8e4e102142e3a36893211f18bc1fe29d054409
Robert Ancell <robert.ancell@gmail.com>
2026-05-18 19:45:37 +0800

    Show preset parameters

--- src/analyze.py
+++ src/analyze.py
@@ -214,6 +214,11 @@ for segment in stream.segments:
     elif isinstance(segment, jpeg.DefineNumberOfLines):
         print("DNL Define Number of Lines")
         print(" Number of lines: %d" % segment.number_of_lines)
+    elif isinstance(segment, jpeg.LSPresetParameters):
+        print("LSE Preset Parameters")
+        print(" Maximum value: %d" % segment.maxval)
+        print(" Gradient thresholds: %d, %d, %d" % (segment.t1, segment.t2, segment.t3))
+        print(" Reset: %d" % segment.reset)
     elif isinstance(segment, jpeg.EndOfImage):
         print("EOI End of Image")
     else:
