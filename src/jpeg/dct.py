2c43e01bc5483a513537aeec0a97ee234a107eb1
Robert Ancell <robert.ancell@gmail.com>
2026-06-01 18:08:53 +1200

    Types and tests

--- src/jpeg/dct.py
+++ src/jpeg/dct.py
@@ -47,7 +47,7 @@ def zig_zag(coefficients: list[T]) -> list[T]:
     return zz
 
 
-def unzig_zag(zz: list[int]) -> list[int]:
+def unzig_zag(zz: list[T]) -> list[T]:
     assert len(zz) == 64
     coordinates = zig_zag_coordinates()
     coefficients = [0] * 64
@@ -58,7 +58,7 @@ def unzig_zag(zz: list[int]) -> list[int]:
 
 def fdct(values: list[int]) -> list[float]:
     C = [0.70710678118654752440, 1, 1, 1, 1, 1, 1, 1]
-    coefficients = []
+    coefficients = [0.0] * 64
     for v in range(8):
         for u in range(8):
             s = 0.0
@@ -69,14 +69,14 @@ def fdct(values: list[int]) -> list[float]:
                         * math.cos((2 * x + 1) * u * math.pi / 16)
                         * math.cos((2 * y + 1) * v * math.pi / 16)
                     )
-            coefficients.append(0.25 * C[u] * C[v] * s)
+            coefficients[v * 8 + u] = 0.25 * C[u] * C[v] * s
 
     return coefficients
 
 
 def idct(coefficients: list[int]) -> list[int]:
     C = [0.70710678118654752440, 1, 1, 1, 1, 1, 1, 1]
-    values = []
+    values = [0] * 64
     for y in range(8):
         for x in range(8):
             s = 0.0
@@ -89,17 +89,27 @@ def idct(coefficients: list[int]) -> list[int]:
                         * math.cos((2 * x + 1) * u * math.pi / 16)
                         * math.cos((2 * y + 1) * v * math.pi / 16)
                     )
-            values.append(round(0.25 * s))
+            values[y * 8 + x] = round(0.25 * s)
 
     return values
 
 
 def quantize(data_unit: list[float], quantization_table: list[int]) -> list[int]:
     assert len(data_unit) == len(quantization_table)
-    quantized_data_unit = []
+    quantized_data_unit = [0] * 64
     for i in range(len(data_unit)):
-        quantized_data_unit.append(round(data_unit[i] / quantization_table[i]))
+        quantized_data_unit[i] = round(data_unit[i] / quantization_table[i])
     return quantized_data_unit
+
+
+def dequantize(
+    quantized_data_unit: list[int], quantization_table: list[int]
+) -> list[int]:
+    assert len(quantized_data_unit) == len(quantization_table)
+    data_unit = [0] * 64
+    for i in range(len(quantized_data_unit)):
+        data_unit[i] = quantized_data_unit[i] * quantization_table[i]
+    return data_unit
 
 
 def order_mcu_dct_data_units(
@@ -118,3 +128,24 @@ def order_mcu_dct_data_units(
                     i = (mcu_y + du_y) * (width // 8) + mcu_x + du_x
                     mcu_data_units.append(data_units[i])
     return mcu_data_units
+
+
+if __name__ == "__main__":
+    import random
+
+    def is_near(a: list[int], b: list[int], tolerance: int = 0) -> bool:
+        if len(a) != len(b):
+            return False
+        for i in range(len(a)):
+            if abs(a[i] - b[i]) > tolerance:
+                return False
+        return True
+
+    samples = [random.randint(0, 255) - 128 for _ in range(64)]
+    coefficients = quantize(fdct(samples), [1] * 64)
+    reconstructed_samples = idct(coefficients)
+    assert is_near(reconstructed_samples, samples, tolerance=1)
+
+    zz_coefficients = zig_zag(coefficients)
+    reconstructed_coefficients = unzig_zag(zz_coefficients)
+    assert reconstructed_coefficients == coefficients
