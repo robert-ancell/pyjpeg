4425c1cd8dc9acc6bfeef5315b593ac60d311a40
Robert Ancell <robert.ancell@gmail.com>
2026-06-14 12:18:42 +1200

    Calculate indexes at startup, rather than a fixed table

--- src/jpeg/dct.py
+++ src/jpeg/dct.py
@@ -8,6 +8,7 @@ def transform_coefficient(coefficient: int, point_transform: int) -> int:
         return -(-coefficient >> point_transform)
 
 
+# Calculate the order that DCT coefficients are stored in the zig-zag pattern.
 def zig_zag_indexes() -> list[tuple[int, int]]:
     x = 0
     y = 0
@@ -34,72 +35,8 @@ def zig_zag_indexes() -> list[tuple[int, int]]:
     return indexes
 
 
-precalculated_zig_zag_indexes = [
-    0,
-    1,
-    8,
-    16,
-    9,
-    2,
-    3,
-    10,
-    17,
-    24,
-    32,
-    25,
-    18,
-    11,
-    4,
-    5,
-    12,
-    19,
-    26,
-    33,
-    40,
-    48,
-    41,
-    34,
-    27,
-    20,
-    13,
-    6,
-    7,
-    14,
-    21,
-    28,
-    35,
-    42,
-    49,
-    56,
-    57,
-    50,
-    43,
-    36,
-    29,
-    22,
-    15,
-    23,
-    30,
-    37,
-    44,
-    51,
-    58,
-    59,
-    52,
-    45,
-    38,
-    31,
-    39,
-    46,
-    53,
-    60,
-    61,
-    54,
-    47,
-    55,
-    62,
-    63,
-]
+# Pre-calculated result of zig_zag_indexes()
+precalculated_zig_zag_indexes = zig_zag_indexes()
 
 
 def zig_zag(coefficients: list[int]) -> list[int]:
@@ -186,8 +123,6 @@ def order_mcu_dct_data_units(
 
 if __name__ == "__main__":
     import random
-
-    assert precalculated_zig_zag_indexes == zig_zag_indexes()
 
     def is_near(a: list[int], b: list[int], tolerance: int = 0) -> bool:
         if len(a) != len(b):
