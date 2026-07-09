bb728f1f44883bc4fad73bee59f87ccf868b6e7e
Robert Ancell <robert.ancell@gmail.com>
2026-07-09 21:55:59 +1200

    Validate dimensions against metadata

--- tests/test_decode_suite.py
+++ tests/test_decode_suite.py
@@ -1,4 +1,5 @@
 import glob
+import json
 import os
 
 import pytest
@@ -9,11 +10,18 @@ TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 JPG_FILES = sorted(glob.glob(os.path.join(TEST_DATA_DIR, "*/*.jpg")))
 
 
+def get_metadata(jpg_path):
+    path = os.path.splitext(jpg_path)[0] + ".json"
+    return json.load(open(path))
+
+
 @pytest.mark.parametrize(
     "jpg_path", JPG_FILES, ids=[os.path.relpath(p, TEST_DATA_DIR) for p in JPG_FILES]
 )
 def test_decodes_without_error(jpg_path):
+    metadata = get_metadata(jpg_path)
     with open(jpg_path, "rb") as f:
-        reader = pyjpeg.FileReader(f)
-        image = pyjpeg.Image.read(reader)
-        assert image
+        image = pyjpeg.Image.read(pyjpeg.FileReader(f))
+
+    assert image.samples_per_line == metadata["width"]
+    assert image.number_of_lines == metadata["height"]
