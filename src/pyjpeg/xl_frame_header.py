40a4ec096c05d2b4b781162648ee760ad9145d1d
Robert Ancell <robert.ancell@gmail.com>
2026-07-18 12:36:26 -0400

    More frame header

--- src/pyjpeg/xl_frame_header.py
+++ src/pyjpeg/xl_frame_header.py
@@ -1,5 +1,4 @@
-from operator import is_
-
+import pyjpeg.io
 import pyjpeg.xl_io
 
 
@@ -23,28 +22,215 @@ class XLFrameFlag:
     SKIP_ADAPTIVE_LF_SMOOTHING = 1 << 7
 
 
+class XLPasses:
+    # FIXME: Combine down_samples and last_pass as they are the same length
+    def __init__(
+        self, shift: list[int] = [0], down_samples: list[tuple[int, int]] = [(1, 0)]
+    ):
+        if len(shift) < 1 or len(shift) > 12:
+            raise ValueError("Invalid number of passes")
+        if shift[-1] != 0:
+            raise ValueError("Last shift must be 0")
+        if len(down_samples) > len(shift):
+            raise ValueError("Invalid number of down samples")
+        if down_samples[-1] != (1, len(shift) - 1):
+            raise ValueError("Invalid last down sample")
+        self.shift = shift
+        self.down_samples = down_samples
+
+    def write(self, writer: pyjpeg.xl_io.XLWriter) -> None:
+        writer.write_u32(len(self.shift) - 1, (1, 2, 3, 4), (0, 0, 0, 3))
+        for shift in self.shift[:-1]:
+            writer.write_bits(shift, 2)
+        for shift, _ in self.down_samples[:-1]:
+            writer.write_bits(shift, 2)
+        for _, last_pass in self.down_samples[:-1]:
+            writer.write_u32(last_pass, (0, 1, 2, 0), (0, 0, 0, 3))
+
+    @classmethod
+    def read(cls, reader: pyjpeg.xl_io.XLReader) -> "XLPasses":
+        number = reader.read_u32((1, 2, 3, 4), (0, 0, 0, 3))
+        number_down_sample = reader.read_u32((0, 1, 2, 3), (0, 0, 0, 1))
+        if number_down_sample >= number:
+            raise pyjpeg.io.ReadError("Invalid number of down samples")
+        shift = []
+        for _ in range(number - 1):
+            shift.append(reader.read_bits(2))
+        shift.append(0)
+        down_sample_shifts = []
+        for _ in range(number_down_sample):
+            down_sample_shifts.append(reader.read_bits(2))
+        down_samples = []
+        for shift in down_sample_shifts:
+            down_samples.append((shift, reader.read_u32((0, 1, 2, 0), (0, 0, 0, 3))))
+        down_samples.append((1, number - 1))
+
+        return cls(shift=shift, down_samples=down_samples)
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, XLPasses)
+            and self.shift == other.shift
+            and self.down_samples == other.down_samples
+        )
+
+    def __repr__(self) -> str:
+        return f"XLPasses(shift={self.shift}, down_samples={self.down_samples})"
+
+
+class XLCropArea:
+    def __init__(
+        self,
+        x: int = 0,
+        y: int = 0,
+        width: int = 0,
+        height: int = 0,
+    ):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def write(self, writer: pyjpeg.xl_io.XLWriter, frame_type: int) -> None:
+        if frame_type != XLFrameType.REFERENCE_ONLY:
+            writer.write_u32(self.x, (0, 256, 2304, 18688), (8, 11, 14, 30))
+            writer.write_u32(self.y, (0, 256, 2304, 18688), (8, 11, 14, 30))
+        writer.write_u32(self.width, (0, 256, 2304, 18688), (8, 11, 14, 30))
+        writer.write_u32(self.height, (0, 256, 2304, 18688), (8, 11, 14, 30))
+
+    @classmethod
+    def read(cls, reader: pyjpeg.xl_io.XLReader, frame_type: int) -> "XLCropArea":
+        if frame_type != XLFrameType.REFERENCE_ONLY:
+            x = reader.read_u32((0, 256, 2304, 18688), (8, 11, 14, 30))
+            y = reader.read_u32((0, 256, 2304, 18688), (8, 11, 14, 30))
+        else:
+            x = 0
+            y = 0
+        width = reader.read_u32((0, 256, 2304, 18688), (8, 11, 14, 30))
+        height = reader.read_u32((0, 256, 2304, 18688), (8, 11, 14, 30))
+        return cls(x, y, width, height)
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, XLCropArea) and (
+            self.x == other.x
+            and self.y == other.y
+            and self.width == other.width
+            and self.height == other.height
+        )
+
+    def __repr__(self) -> str:
+        return f"XLCropArea(x={self.x}, y={self.y}, width={self.width}, height={self.height})"
+
+
+class XLAnimationHeader:
+    def __init__(
+        self,
+        duration: int = 0,
+        timecode: int = 0,
+    ):
+        self.duration = duration
+        self.timecode = timecode
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, XLAnimationHeader) and (
+            self.duration == other.duration and self.timecode == other.timecode
+        )
+
+    def __repr__(self) -> str:
+        return f"XLAnimationHeader(duration={self.duration}, timecode={self.timecode})"
+
+
 class XLFrameHeader:
     def __init__(
         self,
         frame_type: int = XLFrameType.REGULAR,
         encoding: int = XLFrameEncoding.VARDCT,
+        is_xyb_encoded: bool = False,
         flags: int = 0,
+        do_ycbcr: bool = False,
+        upsampling_mode: tuple[int, int, int] = (0, 0, 0),
+        group_size_shift: int = 1,
+        x_qm_scale: int = 0,
+        b_qm_scale: int = 0,
+        if_level: int = 0,
+        passes: XLPasses = XLPasses(),
+        crop_area: XLCropArea | None = None,
+        animation_header: XLAnimationHeader | None = None,
+        is_last: bool = False,
+        name: str = "",
     ):
+        if frame_type == XLFrameType.LF and crop_area is not None:
+            raise ValueError("crop_area must be None for LF frames")
+        if encoding != XLFrameEncoding.MODULAR and group_size_shift != 1:
+            raise ValueError("group_size_shift must be 1 for non-MODULAR frames")
+        is_normal_frame = frame_type in (
+            XLFrameType.REGULAR,
+            XLFrameType.SKIP_PROGRESSIVE,
+        )
+        if not is_normal_frame and is_last:
+            raise ValueError("is_last must be False for non-normal frames")
         self.frame_type = frame_type
         self.encoding = encoding
+        self.is_xyb_encoded = is_xyb_encoded
         self.flags = flags
+        self.do_ycbcr = do_ycbcr
+        self.upsampling_mode = upsampling_mode
+        self.group_size_shift = group_size_shift
+        self.x_qm_scale = x_qm_scale
+        self.b_qm_scale = b_qm_scale
+        self.if_level = if_level
+        self.passes = passes
+        self.crop_area = crop_area
+        self.animation_header = animation_header
+        self.is_last = is_last
+        self.name = name
 
     def write(self, writer: pyjpeg.xl_io.XLWriter):
         is_default = self == XLFrameHeader()
         writer.write_bool(is_default)
         if is_default:
             return
+
         writer.write_bits(self.frame_type, 2)
         writer.write_bits(self.encoding, 1)
         writer.write_u64(self.flags)
+        if not self.is_xyb_encoded:
+            writer.write_bool(self.do_ycbcr)
+            if self.do_ycbcr and (self.flags & XLFrameFlag.USE_LF_FRAME) == 0:
+                for mode in self.upsampling_mode:
+                    writer.write_bits(mode, 2)
+        if self.encoding == XLFrameEncoding.MODULAR:
+            writer.write_bits(self.group_size_shift, 2)
+        if self.is_xyb_encoded and self.encoding == XLFrameEncoding.VARDCT:
+            writer.write_bits(self.x_qm_scale, 3)
+            writer.write_bits(self.b_qm_scale, 3)
+        if self.frame_type != XLFrameType.REFERENCE_ONLY:
+            self.passes.write(writer)
+        if self.frame_type == XLFrameType.LF:
+            writer.write_bits(self.if_level - 1, 2)
+        else:
+            writer.write_bool(self.crop_area is not None)
+            if self.crop_area is not None:
+                self.crop_area.write(writer, self.frame_type)
+        is_normal_frame = self.frame_type in (
+            XLFrameType.REGULAR,
+            XLFrameType.SKIP_PROGRESSIVE,
+        )
+        if is_normal_frame:
+            writer.write_bool(self.is_last)
+        name_bytes = str.encode(self.name, "utf-8")
+        writer.write_u32(len(name_bytes), (0, 0, 16, 48), (0, 4, 5, 10))
+        writer.write_bytes(name_bytes)
 
     @classmethod
-    def read(cls, reader: pyjpeg.xl_io.XLReader, is_xyb_encoded: bool = False):
+    def read(
+        cls,
+        reader: pyjpeg.xl_io.XLReader,
+        is_xyb_encoded: bool = False,
+        channel_count: int = 1,
+        have_animation_header: bool = False,
+        have_timecodes: bool = False,
+    ):
         if reader.read_bool():
             return cls()
 
@@ -53,31 +239,131 @@ class XLFrameHeader:
         flags = reader.read_u64()
         if not is_xyb_encoded:
             do_ycbcr = reader.read_bool()
-            if do_ycbcr and (flags & XLFrameFlag.USE_LF_FRAME) == 0:
-                mode = reader.read_bits(2)
-                pass  # FIXME
+        else:
+            do_ycbcr = False
+        if do_ycbcr and (flags & XLFrameFlag.USE_LF_FRAME) == 0:
+            upsampling_mode = (
+                reader.read_bits(2),
+                reader.read_bits(2),
+                reader.read_bits(2),
+            )
+        else:
+            upsampling_mode = (0, 0, 0)
         if (flags & XLFrameFlag.USE_LF_FRAME) == 0:
-            pass  # FIXME
+            upsampling = []
+            for i in range(channel_count):
+                upsampling.append(1 << reader.read_bits(2))
+        else:
+            upsampling = [1]
         if encoding == XLFrameEncoding.MODULAR:
             group_size_shift = reader.read_bits(2)
         else:
             group_size_shift = 1
         if is_xyb_encoded and encoding == XLFrameEncoding.VARDCT:
-            xqm_scale = reader.read_bits(3)
-            bqm_scale = reader.read_bits(3)
+            x_qm_scale = reader.read_bits(3)
+            b_qm_scale = reader.read_bits(3)
         else:
             # FIXME: Note default is 3, 2 for is_xyb_encoded, otherwise 2,2
-            xqm_scale = 2
-            bqm_scale = 2
+            x_qm_scale = 2
+            b_qm_scale = 2
+        if frame_type != XLFrameType.REFERENCE_ONLY:
+            passes = XLPasses.read(reader)
+        else:
+            passes = XLPasses()
+        if frame_type == XLFrameType.LF:
+            if_level = 1 + reader.read_bits(2)
+            crop_area: XLCropArea | None = None
+        else:
+            if_level = 0
+            if reader.read_bool():
+                crop_area = XLCropArea.read(reader, frame_type)
+            else:
+                crop_area = None
+        is_normal_frame = frame_type in (
+            XLFrameType.REGULAR,
+            XLFrameType.SKIP_PROGRESSIVE,
+        )
+        if is_normal_frame and have_animation_header:
+            duration = reader.read_u32((0, 1, 0, 0), (0, 0, 8, 32))
+            if have_timecodes:
+                timecode = reader.read_bits(32)
+            else:
+                timecode = 0
+            animation_header = XLAnimationHeader(duration=duration, timecode=timecode)
+        else:
+            animation_header = None
+        if is_normal_frame:
+            is_last = reader.read_bool()
+        else:
+            is_last = False
+        name_length = reader.read_u32((0, 0, 16, 48), (0, 4, 5, 10))
+        name = str(reader.read_bytes(name_length), "utf-8")
 
-        return cls(frame_type=frame_type, encoding=encoding, flags=flags)
+        return cls(
+            frame_type=frame_type,
+            encoding=encoding,
+            is_xyb_encoded=is_xyb_encoded,
+            flags=flags,
+            do_ycbcr=do_ycbcr,
+            upsampling_mode=upsampling_mode,
+            group_size_shift=group_size_shift,
+            x_qm_scale=x_qm_scale,
+            b_qm_scale=b_qm_scale,
+            if_level=if_level,
+            passes=passes,
+            crop_area=crop_area,
+            animation_header=animation_header,
+            is_last=is_last,
+            name=name,
+        )
 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, XLFrameHeader) and (
             self.frame_type == other.frame_type
             and self.encoding == other.encoding
+            and self.is_xyb_encoded == other.is_xyb_encoded
             and self.flags == other.flags
+            and self.do_ycbcr == other.do_ycbcr
+            and self.upsampling_mode == other.upsampling_mode
+            and self.group_size_shift == other.group_size_shift
+            and self.x_qm_scale == other.x_qm_scale
+            and self.b_qm_scale == other.b_qm_scale
+            and self.if_level == other.if_level
+            and self.passes == other.passes
+            and self.crop_area == other.crop_area
+            and self.animation_header == other.animation_header
+            and self.is_last == other.is_last
+            and self.name == other.name
         )
 
     def __repr__(self) -> str:
-        return f"XLFrameHeader(frame_type={self.frame_type}, encoding={self.encoding}, flags={self.flags})"
+        args = []
+        if self.frame_type != XLFrameType.REGULAR:
+            args.append(f"frame_type={self.frame_type}")
+        if self.encoding != XLFrameEncoding.VARDCT:
+            args.append(f"encoding={self.encoding}")
+        if self.flags != 0:
+            args.append(f"flags={self.flags}")
+        if self.do_ycbcr:
+            args.append(f"do_ycbcr={self.do_ycbcr}")
+        if self.upsampling_mode != (0, 0, 0):
+            args.append(f"upsampling_mode={self.upsampling_mode}")
+        if self.group_size_shift != 1:
+            args.append(f"group_size_shift={self.group_size_shift}")
+        if self.x_qm_scale != 0:
+            args.append(f"x_qm_scale={self.x_qm_scale}")
+        if self.b_qm_scale != 0:
+            args.append(f"b_qm_scale={self.b_qm_scale}")
+        if self.if_level != 0:
+            args.append(f"if_level={self.if_level}")
+        if self.passes != XLPasses():
+            args.append(f"passes={self.passes}")
+        if self.crop_area is not None:
+            args.append(f"crop_area={self.crop_area}")
+        if self.animation_header is not None:
+            args.append(f"animation_header={self.animation_header}")
+        if self.is_last:
+            args.append(f"is_last={self.is_last}")
+        if self.name:
+            args.append(f"name={self.name}")
+        return f"XLFrameHeader({', '.join(args)})"
